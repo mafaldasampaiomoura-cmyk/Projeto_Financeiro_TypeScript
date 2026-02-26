868b3db8fc388a8c51dce1855d95970a88528997
Mafalda Moura <mafaldamoura@Mac.lan>
2026-02-26 22:45:26 +0000

    atualização final do projeto com as últimas que faziam parte

--- dist/UserInterface/userInterface.d.ts
+++ dist/UserInterface/userInterface.d.ts
@@ -1,4 +1,4 @@
 export declare function renderTransactions(): void;
 export declare function atualizacaoCards(lista?: import("../transactions/transactions.js").Transaction[]): void;
-export declare function formatacaoEuro(valor: any): any;
+export declare function formatacaoEuro(valor: number): string;
 //# sourceMappingURL=userInterface.d.ts.map
